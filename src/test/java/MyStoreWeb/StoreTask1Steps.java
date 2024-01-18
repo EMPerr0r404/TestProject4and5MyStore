ee272156eb7e33b1ff0f32d6b51f9c33a0e17bb6
EMPerr0r404 <126023388+EMPerr0r404@users.noreply.github.com>
2024-01-18 18:05:39 +0100

    Update StoreTask1Steps.java

--- src/test/java/MyStoreWeb/StoreTask1Steps.java
+++ src/test/java/MyStoreWeb/StoreTask1Steps.java
@@ -4,7 +4,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-// import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -15,18 +15,18 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.time.Duration;
 
-// import static org.hamcrest.CoreMatchers.containsString;
-// import static org.hamcrest.MatcherAssert.assertThat;
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StoreTask1Steps {
     // WebDriver instance and page objects initialization
     public WebDriver driver;
     private StoreLogInPage logInPage;
     private StoreMainPage mainPage;
-    // private StoreYourAccPage accPage;
-    // private StoreYourAddressesPage addressesPage;
-    // private StoreNewAddressPage newAddressPage;
+    private StoreYourAccPage accPage;
+    private StoreYourAddressesPage addressesPage;
+    private StoreNewAddressPage newAddressPage;
 
     // Step for opening the page in the browser
     @Given("Page {string} opened in browser")
@@ -37,6 +37,7 @@ public class StoreTask1Steps {
         this.driver = new ChromeDriver(options);
            
         // FirefoxOptions options = new FirefoxOptions();
+        // options.addArguments("--headless");
         // driver = new FirefoxDriver(options);
 
 
@@ -50,23 +51,7 @@ public class StoreTask1Steps {
         // this.addressesPage = new StoreYourAddressesPage(driver);
         // this.newAddressPage = new StoreNewAddressPage(driver);
 
-        // --------------------------------
 
-        // ChromeOptions options = new ChromeOptions();
-        // options.addArguments("--headless");
-        // this.driver = new ChromeDriver(options);
- 
-
- 
-        // FirefoxOptions options = new FirefoxOptions();
-        // options.addArguments("--headless");
-        // this.driver = new FirefoxDriver(options);
-    
-     
-        // this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
-        // this.driver.get(url);
-        // this.logInPage = new StoreLogInPage(driver);
-        // this.mainPage = new StoreMainPage(driver);
     }
     @When("Button SignIn clicked")
     public void buttonSignIn() {
@@ -74,126 +59,126 @@ public class StoreTask1Steps {
     }
 
 
-    // @And("Email filled")
-    // public void emailFilled() {
-    //     logInPage.setFieldEmail();
-    // }
+    @And("Email filled")
+    public void emailFilled() {
+        logInPage.setFieldEmail();
+    }
 
-    // @And("Password filled")
-    // public void passwordFilled() {
-    //     logInPage.setFieldPass();
-    // }
+    @And("Password filled")
+    public void passwordFilled() {
+        logInPage.setFieldPass();
+    }
 
-    // @And("Button Sign clicked")
-    // public void buttonSignClicked() {
-    //     logInPage.submit();
-    // }
+    @And("Button Sign clicked")
+    public void buttonSignClicked() {
+        logInPage.submit();
+    }
 
-    // @And("Address clicked")
-    // public void addressClicked() {
-    //     accPage.clickAddresses();
-    // }
+    @And("Address clicked")
+    public void addressClicked() {
+        accPage.clickAddresses();
+    }
 
-    // @And("Create new address clicked")
-    // public void createNewAddressClicked() {
-    //     addressesPage.clickCreateNew();
-    // }
+    @And("Create new address clicked")
+    public void createNewAddressClicked() {
+        addressesPage.clickCreateNew();
+    }
 
-    // @And("Alias {string} field filled")
-    // public void aliasFieldFilled(String alias) {
-    //     newAddressPage.setFieldAlias(alias);
-    // }
+    @And("Alias {string} field filled")
+    public void aliasFieldFilled(String alias) {
+        newAddressPage.setFieldAlias(alias);
+    }
 
-    // @And("Address {string} field filled")
-    // public void addressAddressFieldFilled(String address) {
-    //     newAddressPage.setFieldAddress(address);
-    // }
+    @And("Address {string} field filled")
+    public void addressAddressFieldFilled(String address) {
+        newAddressPage.setFieldAddress(address);
+    }
 
-    // @And("City {string} field filled")
-    // public void cityCityFieldFilled(String city) {
-    //     newAddressPage.setFieldCity(city);
-    // }
+    @And("City {string} field filled")
+    public void cityCityFieldFilled(String city) {
+        newAddressPage.setFieldCity(city);
+    }
 
-    // @And("ZipCode {string} field filled")
-    // public void zipcodeZipCodeFieldFilled(String zipCode) {
-    //     newAddressPage.setFieldPostcode(zipCode);
-    // }
+    @And("ZipCode {string} field filled")
+    public void zipcodeZipCodeFieldFilled(String zipCode) {
+        newAddressPage.setFieldPostcode(zipCode);
+    }
 
-    // @And("Phone {string} field filled")
-    // public void phonePhoneFieldFilled(String phone) {
-    //     newAddressPage.setFieldPhone(phone);
-    // }
+    @And("Phone {string} field filled")
+    public void phonePhoneFieldFilled(String phone) {
+        newAddressPage.setFieldPhone(phone);
+    }
 
-    // @And("Save button clicked")
-    // public void saveButtonClicked() {
-    //     newAddressPage.clickSave();
-    // }
+    @And("Save button clicked")
+    public void saveButtonClicked() {
+        newAddressPage.clickSave();
+    }
 
-    // @Then("Check if address expectedAlias equals {string}")
-    // public void checkIfAddressEquals(String expectedAlias) {
-    //     // Getting the actual Alias value from the page and asserting it
-    //     String actualAlias = addressesPage.getAlias2();
-    //     assertEquals(expectedAlias, actualAlias);
-    //     // Verifying if the actual Alias contains the expected value, otherwise, fail the assertion
-    //     if (!actualAlias.contains(expectedAlias)) {
-    //         Assertions.fail(String.format("Search result should contain: %s, but was: %s", expectedAlias, actualAlias));
-    //     }
-    // }
+    @Then("Check if address expectedAlias equals {string}")
+    public void checkIfAddressEquals(String expectedAlias) {
+        // Getting the actual Alias value from the page and asserting it
+        String actualAlias = addressesPage.getAlias2();
+        assertEquals(expectedAlias, actualAlias);
+        // Verifying if the actual Alias contains the expected value, otherwise, fail the assertion
+        if (!actualAlias.contains(expectedAlias)) {
+            Assertions.fail(String.format("Search result should contain: %s, but was: %s", expectedAlias, actualAlias));
+        }
+    }
 
-    // @Then("Check if address Address equals {string}")
-    // public void checkIfAddressEqualsAddress(String expectedAddress) {
-    //     // Getting the actual Address value from the page and asserting it
-    //     String actualAddress = addressesPage.getAddress2();
-    //     assertThat(actualAddress, containsString(expectedAddress));
-    //     // Verifying if the actual Address contains the expected value, otherwise, fail the assertion
-    //     if (!actualAddress.contains(expectedAddress)) {
-    //         Assertions.fail(String.format("Address should contain: %s, but was: %s", expectedAddress, actualAddress));
-    //     }
-    // }
+    @Then("Check if address Address equals {string}")
+    public void checkIfAddressEqualsAddress(String expectedAddress) {
+        // Getting the actual Address value from the page and asserting it
+        String actualAddress = addressesPage.getAddress2();
+        assertThat(actualAddress, containsString(expectedAddress));
+        // Verifying if the actual Address contains the expected value, otherwise, fail the assertion
+        if (!actualAddress.contains(expectedAddress)) {
+            Assertions.fail(String.format("Address should contain: %s, but was: %s", expectedAddress, actualAddress));
+        }
+    }
 
-    // @Then("Check if address City equals {string}")
-    // public void checkIfAddressCityEqualsCity(String expectedCity) {
-    //     String actualCity = addressesPage.getAddress2();
-    //     assertThat(actualCity, containsString(expectedCity));
-    //     if (!actualCity.contains(expectedCity)) {
-    //         Assertions.fail(String.format("City should contain: %s, but was: %s", expectedCity, actualCity));
-    //     }
-    // }
+    @Then("Check if address City equals {string}")
+    public void checkIfAddressCityEqualsCity(String expectedCity) {
+        String actualCity = addressesPage.getAddress2();
+        assertThat(actualCity, containsString(expectedCity));
+        if (!actualCity.contains(expectedCity)) {
+            Assertions.fail(String.format("City should contain: %s, but was: %s", expectedCity, actualCity));
+        }
+    }
 
-    // @Then("Check if address Phone equals {string}")
-    // public void checkIfAddressPhoneEqualsPhone(String expectedPhone) {
-    //     String actualPhone = addressesPage.getAddress2();
-    //     assertThat(actualPhone, containsString(expectedPhone));
-    //     if (!actualPhone.contains(expectedPhone)) {
-    //         Assertions.fail(String.format("Phone should contain: %s, but was: %s", expectedPhone, actualPhone));
-    //     }
-    // }
+    @Then("Check if address Phone equals {string}")
+    public void checkIfAddressPhoneEqualsPhone(String expectedPhone) {
+        String actualPhone = addressesPage.getAddress2();
+        assertThat(actualPhone, containsString(expectedPhone));
+        if (!actualPhone.contains(expectedPhone)) {
+            Assertions.fail(String.format("Phone should contain: %s, but was: %s", expectedPhone, actualPhone));
+        }
+    }
 
-    // @Then("Check if address ZipCode equals {string}")
-    // public void checkIfAddressZipCodeEqualsZipCode(String expectedZipCode) {
-    //     String actualZipCode = addressesPage.getAddress2();
-    //     assertThat(actualZipCode, containsString(expectedZipCode));
-    //     if (!actualZipCode.contains(expectedZipCode)) {
-    //         Assertions.fail(String.format("ZipCode should contain: %s, but was: %s", expectedZipCode, actualZipCode));
-    //     }
-    // }
+    @Then("Check if address ZipCode equals {string}")
+    public void checkIfAddressZipCodeEqualsZipCode(String expectedZipCode) {
+        String actualZipCode = addressesPage.getAddress2();
+        assertThat(actualZipCode, containsString(expectedZipCode));
+        if (!actualZipCode.contains(expectedZipCode)) {
+            Assertions.fail(String.format("ZipCode should contain: %s, but was: %s", expectedZipCode, actualZipCode));
+        }
+    }
 
 
-    // @Then("Delete address")
-    // public void deleteAddress() {
-    //     addressesPage.deleteAddress2();
+    @Then("Delete address")
+    public void deleteAddress() {
+        addressesPage.deleteAddress2();
 
-    // }
+    }
 
-    // @Then("Check if address deleted")
-    // public void checkIfAddressDeleted() {
-    //     String expectedNotification = "Address successfully deleted!";
-    //     String actualNotification = addressesPage.getNotification().getText();
-    //     assertEquals(expectedNotification, actualNotification);
-    //     if (!actualNotification.contains(expectedNotification)) {
-    //         Assertions.fail(String.format("Expected notification: %s, but was: %s", expectedNotification, actualNotification));
-    //     }
-    // }
+    @Then("Check if address deleted")
+    public void checkIfAddressDeleted() {
+        String expectedNotification = "Address successfully deleted!";
+        String actualNotification = addressesPage.getNotification().getText();
+        assertEquals(expectedNotification, actualNotification);
+        if (!actualNotification.contains(expectedNotification)) {
+            Assertions.fail(String.format("Expected notification: %s, but was: %s", expectedNotification, actualNotification));
+        }
+    }
 
     @Then("Close browser")
     public void closeBrowser() {
