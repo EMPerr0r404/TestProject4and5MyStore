1679f0998f668c1476c3363c2ab6d7d870e8aea6
EMPerr0r404 <126023388+EMPerr0r404@users.noreply.github.com>
2024-01-18 18:10:29 +0100

    Update StoreYourAccPage.java

--- src/main/java/MyStoreWeb/StoreYourAccPage.java
+++ src/main/java/MyStoreWeb/StoreYourAccPage.java
@@ -1,35 +1,35 @@
-// package MyStoreWeb;
+package MyStoreWeb;
 
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.WebElement;
-// import org.openqa.selenium.support.FindBy;
-// import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-// public class StoreYourAccPage {
+public class StoreYourAccPage {
 
-//     @FindBy(id = "addresses-link")
-//     private WebElement addresses;
+    @FindBy(id = "addresses-link")
+    private WebElement addresses;
 
-//     @FindBy(css = "#_desktop_logo > a")
-//     private WebElement myStore;
+    @FindBy(css = "#_desktop_logo > a")
+    private WebElement myStore;
 
-//     @FindBy(css = "#history-link")
-//     private WebElement history;
+    @FindBy(css = "#history-link")
+    private WebElement history;
 
 
-//     public StoreYourAccPage(WebDriver driver) {
-//         PageFactory.initElements(driver, this);
-//     }
+    public StoreYourAccPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
 
-//     public void clickAddresses() {
-//         addresses.click();
-//     }
-//     public void clickMyStore() {
-//         myStore.click();
-//     }
+    public void clickAddresses() {
+        addresses.click();
+    }
+    public void clickMyStore() {
+        myStore.click();
+    }
 
-//     public void clickHistory() {
-//         history.click();
-//     }
+    public void clickHistory() {
+        history.click();
+    }
 
-// }
+}
