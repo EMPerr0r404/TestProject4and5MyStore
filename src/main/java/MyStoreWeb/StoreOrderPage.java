4306cbb206dce31a0a53a93dc8d5b4689048bcfd
EMPerr0r404 <126023388+EMPerr0r404@users.noreply.github.com>
2024-01-18 18:10:11 +0100

    Update StoreOrderPage.java

--- src/main/java/MyStoreWeb/StoreOrderPage.java
+++ src/main/java/MyStoreWeb/StoreOrderPage.java
@@ -1,56 +1,56 @@
-// package MyStoreWeb;
+package MyStoreWeb;
 
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.WebElement;
-// import org.openqa.selenium.support.FindBy;
-// import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-// public class StoreOrderPage {
+public class StoreOrderPage {
 
-//     @FindBy(xpath = "//*[@id=\"checkout-addresses-step\"]/div/div/form/div[2]/button")
-//     private WebElement continue2;
+    @FindBy(xpath = "//*[@id=\"checkout-addresses-step\"]/div/div/form/div[2]/button")
+    private WebElement continue2;
 
-//     @FindBy(css = "#js-delivery > div > div.delivery-options > div:nth-child(1) > div > span > span")
-//     private WebElement selfPickUp;
+    @FindBy(css = "#js-delivery > div > div.delivery-options > div:nth-child(1) > div > span > span")
+    private WebElement selfPickUp;
 
-//     @FindBy(css = "#js-delivery > button")
-//     private WebElement continue3;
+    @FindBy(css = "#js-delivery > button")
+    private WebElement continue3;
 
-//     @FindBy(css = "#payment-option-1")
-//     private WebElement payByCheck;
+    @FindBy(css = "#payment-option-1")
+    private WebElement payByCheck;
 
-//     @FindBy(id = "conditions_to_approve[terms-and-conditions]")
-//     private WebElement termsAgreement;
+    @FindBy(id = "conditions_to_approve[terms-and-conditions]")
+    private WebElement termsAgreement;
 
-//     @FindBy(css = "#payment-confirmation > div.ps-shown-by-js > button")
-//     private WebElement placeOrder;
-
-
-//     public void clickSelfPickUp() {
-//         selfPickUp.click();
-//     }
-//     public void clickContinue3() {
-//         continue3.click();
-//     }
-
-//     public StoreOrderPage(WebDriver driver) {
-//         PageFactory.initElements(driver, this);
-//     }
-
-//     public void clickContinue2() {
-//         continue2.click();
-//     }
-
-//     public void clickPayByCheck() {
-//         payByCheck.click();
-//     }
-//     public void clickTermsAgreement() {
-//         termsAgreement.click();
-//     }
-//     public void clickPlaceOrder() {
-//         placeOrder.click();
-//     }
+    @FindBy(css = "#payment-confirmation > div.ps-shown-by-js > button")
+    private WebElement placeOrder;
 
 
+    public void clickSelfPickUp() {
+        selfPickUp.click();
+    }
+    public void clickContinue3() {
+        continue3.click();
+    }
 
-// }
+    public StoreOrderPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickContinue2() {
+        continue2.click();
+    }
+
+    public void clickPayByCheck() {
+        payByCheck.click();
+    }
+    public void clickTermsAgreement() {
+        termsAgreement.click();
+    }
+    public void clickPlaceOrder() {
+        placeOrder.click();
+    }
+
+
+
+}
