77f7645f8988fca7939cd680891a8b5522c5d2a2
EMPerr0r404 <126023388+EMPerr0r404@users.noreply.github.com>
2024-01-18 17:34:05 +0100

    Update StoreShoppingCartPage.java

--- src/main/java/MyStoreWeb/StoreShoppingCartPage.java
+++ src/main/java/MyStoreWeb/StoreShoppingCartPage.java
@@ -1,23 +1,23 @@
-package MyStoreWeb;
+// package MyStoreWeb;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.WebElement;
+// import org.openqa.selenium.support.FindBy;
+// import org.openqa.selenium.support.PageFactory;
 
-public class StoreShoppingCartPage {
+// public class StoreShoppingCartPage {
 
-    @FindBy(css = "a.btn.btn-primary")
-    private WebElement proceedToCheckout2;
-
-
-    public StoreShoppingCartPage(WebDriver driver) {
-        PageFactory.initElements(driver, this);
-    }
-
-    public void clickProceedToCheckout2() {
-        proceedToCheckout2.click();
-    }
+//     @FindBy(css = "a.btn.btn-primary")
+//     private WebElement proceedToCheckout2;
 
 
-}
+//     public StoreShoppingCartPage(WebDriver driver) {
+//         PageFactory.initElements(driver, this);
+//     }
+
+//     public void clickProceedToCheckout2() {
+//         proceedToCheckout2.click();
+//     }
+
+
+// }
