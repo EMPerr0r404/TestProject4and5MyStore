27db116d45462de3881eb80ff461825d2a36d7b3
EMPerr0r404 <126023388+EMPerr0r404@users.noreply.github.com>
2024-01-18 16:20:00 +0100

    Update StoreTask2Steps.java

--- src/test/java/MyStoreWeb/StoreTask2Steps.java
+++ src/test/java/MyStoreWeb/StoreTask2Steps.java
@@ -7,7 +7,12 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.io.File;
 import java.io.IOException;
