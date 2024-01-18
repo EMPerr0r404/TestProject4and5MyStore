4441fad996f1062cfb00f04d10fbcdc797523f95
EMPerr0r404 <126023388+EMPerr0r404@users.noreply.github.com>
2024-01-18 17:38:42 +0100

    Update StoreMainPage.java

--- src/main/java/MyStoreWeb/StoreMainPage.java
+++ src/main/java/MyStoreWeb/StoreMainPage.java
@@ -1,33 +1,33 @@
-// package MyStoreWeb;
+package MyStoreWeb;
 
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.WebElement;
-// import org.openqa.selenium.support.FindBy;
-// import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-// // Page object class representing the main page
-// public class StoreMainPage {
+// Page object class representing the main page
+public class StoreMainPage {
 
-//     // WebElement representing the "Sign In" link on the page
-//     @FindBy(xpath = "//*[@id=\"_desktop_user_info\"]/div/a")
-//     private WebElement signIn;
+    // WebElement representing the "Sign In" link on the page
+    @FindBy(xpath = "//*[@id=\"_desktop_user_info\"]/div/a")
+    private WebElement signIn;
 
-//     // WebElement representing a product (Hummingbird Sweater) on the page
-//     @FindBy(css = "#content > section > div > div:nth-child(2) > article > div > div.thumbnail-top > a")
-//     private WebElement hummingBirdSweater;
+    // WebElement representing a product (Hummingbird Sweater) on the page
+    @FindBy(css = "#content > section > div > div:nth-child(2) > article > div > div.thumbnail-top > a")
+    private WebElement hummingBirdSweater;
 
-//     // Constructor initializing WebElements when a new instance of this class is created
-//     public StoreMainPage(WebDriver driver) {
-//         PageFactory.initElements(driver, this);
-//     }
+    // Constructor initializing WebElements when a new instance of this class is created
+    public StoreMainPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
 
-//     // Method to click the "Sign In" link on the page
-//     public void clickSignIn() {
-//         signIn.click();
-//     }
+    // Method to click the "Sign In" link on the page
+    public void clickSignIn() {
+        signIn.click();
+    }
 
-//     // Method to click on the Hummingbird Sweater product on the page
-//     public void clickHummingSweater() {
-//         hummingBirdSweater.click();
-//     }
-// }
+    // Method to click on the Hummingbird Sweater product on the page
+    public void clickHummingSweater() {
+        hummingBirdSweater.click();
+    }
+}
